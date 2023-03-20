6e63198e376072500be62e0e9d0a8ea8035fd428
Junseong kim <anima94@kakao.com>
2023-03-20 21:38:59 +0900

    feat : publisher project 기본 세팅

---
+++ README.md
@@ -0,0 +1,7 @@
+# KafkaStreams-grafana-elasticsearch
+ KafkaStreams, Grafana, KafkaConnect With ElasticSearch 실습해보는 프로젝트
+
+
+![image](https://user-images.githubusercontent.com/40031858/226338575-405c44cd-db2c-4f5c-8abf-92e75b43f372.png)
+
+
---
+++ checkoutProducer/src/main/kotlin/me/saechimdaeki/checkoutproducer/CheckoutProducerApplication.kt
@@ -0,0 +1,11 @@
+package me.saechimdaeki.checkoutproducer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CheckoutProducerApplication
+
+fun main(args: Array<String>) {
+    runApplication<CheckoutProducerApplication>(*args)
+}
