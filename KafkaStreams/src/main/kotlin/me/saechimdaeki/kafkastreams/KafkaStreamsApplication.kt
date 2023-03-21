b218ceb09e39a5aa7032f8e11380dce96352af6d
Junseong kim <anima94@kakao.com>
2023-03-22 00:13:34 +0900

    feat: KafkaStreams Project 기본세팅

---
+++ KafkaStreams/src/main/kotlin/me/saechimdaeki/kafkastreams/KafkaStreamsApplication.kt
@@ -0,0 +1,11 @@
+package me.saechimdaeki.kafkastreams
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KafkaStreamsApplication
+
+fun main(args: Array<String>) {
+    runApplication<KafkaStreamsApplication>(*args)
+}
